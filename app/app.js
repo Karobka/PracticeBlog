6de15dc812cfaff6b13e8c5a832666c7a39b7a4c
Tim Wright <mithgirw@gmail.com>
2016-06-28 17:51:48 -0600

    adding new posts to array works

--- app/app.js
+++ app/app.js
@@ -18,15 +18,24 @@ $(document).ready(function() {
     ];
     
 
+
+    /** interface to create new post */
     $("#createpostmodal").on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('whatever') // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
-  modal.find('.modal-title').text('New post')
-  modal.find('.modal-body input').val(recipient)
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('whatever') // Extract info from data-* attributes
 });
+ 
+
+    $("#newpostbtn").click(function() {
+        var newPostTitle = $("#post-title").val();
+        var newPostText = $("#post-text").val();
+        if (newPostText == "" || newPostTitle == ""){
+            alert("Can't submit blank post");
+        }else {
+            postsarray.push({postTitle: newPostTitle, postDate:NaN/** change this */, postText:newPostText});
+            console.log(postsarray);
+        }
+    });
 
 
 
