e42de419fa611c3e28aa907258dd1f169a9e6d05
Tim Wright <mithgirw@gmail.com>
2016-06-28 10:25:52 -0600

    starting the javascript

---
+++ app/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    var postsarray = [
+        {
+            title: "Post Number 1",
+            postDate: "June 1, 2016",
+            postText: "Blah blah blah" 
+        },
+        {
+            title: "Post Number 2",
+            postDate: "May 2, 2016",
+            postText: "More more more"
+        },
+        {
+            title: "Post Number 3",
+            postDate: "April 3, 2016",
+            postText: "Another Another Another"
+        }
+    ];
+
+});
