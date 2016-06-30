939a052e08ea0b2480dc349fd014b8de5e514b8c
Tim Wright <mithgirw@gmail.com>
2016-06-30 14:48:13 -0600

    log in and footer work

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 "use strict";
+var userName = $("#user-name").val();
 
 $(document).ready(function() {
     var createId = (function() {
@@ -8,28 +9,55 @@ $(document).ready(function() {
             return postId;
         };
     }());
+
     
-    var postsarray = [];
+    $("#btn-log-in").click(function() {
+        userName = $("#user-name").val();
+        $("#btn-start-login").css("display", "none");
+        $("#btn-create-post").css("display", "block");
+        $("#about-name").append(userName);
+    });
+    
+    var postsarray = [
+        {
+            postId: "5",
+            postTitle: "Post Number 1",
+            postDate: "June 1, 2016",
+            postText: "Blah blah blah" 
+        },
+        {
+            postId: "6",
+            postTitle: "Post Number 2",
+            postDate: "May 2, 2016",
+            postText: "More more more"
+        },
+        {
+            postId: "7",
+            postTitle: "Post Number 3",
+            postDate: "April 3, 2016",
+            postText: "Another Another Another"
+        }
+    ];
 
     function NewPost(postId, postTitle, postDate, postAuthor, postText) {
         this.postId = postId,
         this.postTitle = postTitle,
         this.postDate = postDate,
-        this.postAuthor = postAuthor,
+        this.postAuthor = userName,
         this.postText = postText
     };
 
    NewPost.prototype.createPost = function() {
        $(".blog-post").append(
                 "<h2 class='blog-post-title' value:'" + this.postId + "'>" + this.postTitle + "</h2>"  +
-                "<p class='blog-post-meta'>" + this.postDate + "</p>" + "<hr>" +
+                "<p class='blog-post-meta'>" + this.postDate + " Posted by: " + userName + "</p>" + "<hr>" +
                 "<p>" + this.postText + "</p>"
             );
             /** add new post title to post listing  and include post ID*/
             $("#post-list").after("<a href='#' class='list-group-item' value:'" + this.postId + "'>" + this.postTitle + "</a>");
         };
     
-    /** bootstrap interface to create new post */
+    /** bootstrap modal interface to create new post */
     $("#createpostmodal").on('show.bs.modal');
 
 
@@ -44,17 +72,24 @@ $(document).ready(function() {
             alert("Can't submit a blank post");
         }else {
             $(".blog-post").children().remove();
-            var newerPost = new NewPost(createId(), $("#post-title").val(), Date(), $("#post-title").val(), $("#post-text").val() )
+            var newerPost = new NewPost(createId(), $("#post-title").val(), Date(), userName, $("#post-text").val() )
             newerPost.createPost();
             postsarray.push(newerPost);
             console.log(postsarray); 
         }
     });
 
-    /**update body with clicked on post  Append or replace ".blog-post" (a child of ".blog-main") with content from clicked link */
-    $(".list-group-item").click(function(postId) {        
-        var newnumber = postsarray.find(postId);
-        console.log(newnumber);
+    /**update body with matching postID content for clicked post   */
+    $(".list-group-item").click(function() {        
+        /** var newnumber = postsarray.find(postId);
+        /** console.log(newnumber); */
+        for (var prop in postsarray){
+            console.log(postId[prop]);
+        }
+        for (var i=0; i < postsarray.length; i++) {
+            console.log(postsarray[{postId:['prop']}]);
+        }
+
         
         $(".blog-post").children().remove();
         $(".blog-post").append(
